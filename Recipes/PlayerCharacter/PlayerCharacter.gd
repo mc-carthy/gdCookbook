fc709057052015e73d2344e301a9430ab68b9677
Mick McCarthy <mick@mccarthy.tech>
2020-05-19 23:57:40 +0100

    Camera control cancel and view angle clamping

--- Recipes/PlayerCharacter/PlayerCharacter.gd
+++ Recipes/PlayerCharacter/PlayerCharacter.gd
@@ -6,14 +6,18 @@ var move_speed: float = 10.0
 var direction: Vector3 = Vector3()
 var velocity: Vector3 = Vector3()
 var mouse_sensitivity: float = 0.3
+var max_vertical_camera_angle: float = 60
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed('ui_cancel'):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	if event is InputEventMouseMotion:
 		rotate_y(deg2rad(-event.relative.x * mouse_sensitivity))
 		head_pivot.rotate_x(deg2rad(-event.relative.y * mouse_sensitivity))
+		head_pivot.rotation.x = clamp(head_pivot.rotation.x, -deg2rad(max_vertical_camera_angle), deg2rad(max_vertical_camera_angle))
 
 func _process(delta: float) -> void:
 	direction = Vector3()
