559421094878f22be3bed510f95118551e678325
Mick McCarthy <mick@mccarthy.tech>
2020-05-19 23:53:35 +0100

    Add camera control

--- Recipes/PlayerCharacter/PlayerCharacter.gd
+++ Recipes/PlayerCharacter/PlayerCharacter.gd
@@ -1,8 +1,19 @@
 extends KinematicBody
 
+onready var head_pivot: Spatial = $HeadPivot
+
 var move_speed: float = 10.0
 var direction: Vector3 = Vector3()
 var velocity: Vector3 = Vector3()
+var mouse_sensitivity: float = 0.3
+
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		rotate_y(deg2rad(-event.relative.x * mouse_sensitivity))
+		head_pivot.rotate_x(deg2rad(-event.relative.y * mouse_sensitivity))
 
 func _process(delta: float) -> void:
 	direction = Vector3()
